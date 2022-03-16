2db6f5f587adc4a2eded627bcb0a120022eeda1e
Sumon Chandra Shil <topu199041@gmail.com>
2022-03-16 18:53:36 +0600

    Added portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,125 @@
-import React from 'react';
-import "./Portfolio.css"
+import React from "react";
+import "./Portfolio.css";
+import portfolioImg from "../../assets/portfolio.jpg";
 
 const Portfolio = () => {
-    return (
-        <section id='portfolio'>
-            <h3>This is portfolio</h3>
-        </section>
-    );
+  return (
+    <section id="portfolio">
+      <h5>My recent works</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={portfolioImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https//:github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https//:github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={portfolioImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https//:github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https//:github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={portfolioImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https//:github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https//:github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={portfolioImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https//:github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https//:github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={portfolioImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https//:github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https//:github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={portfolioImg} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio_item-cta">
+            <a href="https//:github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https//:github.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,7 +4,18 @@ import "./Services.css"
 const Services = () => {
     return (
         <section id='services'>
-            <h3>This is services section</h3>
+            <h5>What I offer</h5>
+            <h2>Services</h2>
+
+            <div className="container services_container">
+                <article className="service">
+                    <div className="service_head">
+                        <h3>UI/UX Design</h3>
+
+                    </div>
+                    <li></li>
+                </article>
+            </div>
         </section>
     );
 };
