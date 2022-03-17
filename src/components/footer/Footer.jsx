89c7c64240dfd190b30412e0f0307a533606845a
Sumon Chandra Shil <topu199041@gmail.com>
2022-03-17 23:35:06 +0600

    Added footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import "./Footer.css"
+import "./Footer.css";
+import { BsFacebook } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
     return (
         <footer id='footer'>
-            <h3>This is footer</h3>
+            <a href="#" className='footer_logo'>SUMON CHANDRA SHIL</a>
+
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#service">Service</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer_socials">
+                <a href="https://facebook.com">
+                    <BsFacebook />
+                </a>
+                <a href="https://github.com">
+                    <BsGithub />
+                </a>
+                <a href="https://twitter.com">
+                    <BsTwitter />
+                </a>
+                <a href="https://linkedin.com">
+                    <BsLinkedin />
+                </a>
+            </div>
+            <div className="footer_copyright">
+                <small>&copy; Copyright Sumon Chandra Shil 2022.</small>
+            </div>
         </footer>
     );
 };
